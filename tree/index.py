53991b22beba77c31ff10dfa1761954a99e8c9d2
J. Nick Baughan <JNickBaughan@Gmail.com>
2024-01-28 10:36:15 -0500

    add url for udemy course on python

--- tree/index.py
+++ tree/index.py
@@ -1,3 +1,4 @@
+-- https://www.udemy.com/course/learn-python-by-building-a-blockchain-cryptocurrency/learn/lecture/10047038#overview
 class TreeNode:
     def __init__(self, data):
         self.data = data
